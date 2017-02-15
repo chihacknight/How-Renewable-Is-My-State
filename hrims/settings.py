444f1b453fbd22e1b3b9dece4c7e77bc68bda6fb
Eric Sherman <ericandrewsherman@gmail.com>
2017-02-15 16:51:43 -0600

    will this work

--- hrims/settings.py
+++ hrims/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -138,15 +139,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = "/static/"
+# STATIC_ROOT = "/static/"
+#
+# STATIC_URL = '/static/'
+#
+# STATICFILES_DIRS = [
+#     ('js', './js'),
+#     ('css', './js')
+# ]
+#
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    ('js', './js'),
-    ('css', './js')
-]
-
-
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 STATIC_URL = '/static/'
