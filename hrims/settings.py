992befb5c09f2a2c72925a3d5d5d78869ebf1dea
Eric Sherman <ericandrewsherman@gmail.com>
2017-02-15 12:03:49 -0600

    delete large geojson, install django restframework

--- hrims/settings.py
+++ hrims/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     're_stats.apps.ReStatsConfig'
 ]
 
@@ -92,6 +93,11 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAdminUser',
+    ]
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
