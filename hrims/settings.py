d12b43e0c2bc07c5e0a948bae37d4d5db0474c80
Eric Sherman <ericandrewsherman@gmail.com>
2017-02-15 20:33:43 -0600

    tweak filter

--- hrims/settings.py
+++ hrims/settings.py
@@ -107,8 +107,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ),
-    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+    )
+
 }
 
 # Password validation
