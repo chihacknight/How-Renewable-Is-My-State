266a39db0e3ebd4accd220e81286b611b6a96ddc
Eric Sherman <ericandrewsherman@gmail.com>
2017-02-17 14:17:48 -0600

    mode api to api endpoint

--- hrims/urls.py
+++ hrims/urls.py
@@ -26,7 +26,7 @@ router.register(r'data', views.DataViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'^api/', include(router.urls)),
     url(r'^admin/', admin.site.urls),
     url('^schema/$', schema_view),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
