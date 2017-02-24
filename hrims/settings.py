30d811e51034f416269eef6d118381f3398c74bd
Eric Sherman <ericandrewsherman@gmail.com>
2017-02-24 16:13:48 -0600

    collecting static files into a directory, serving vue via django template

--- hrims/settings.py
+++ hrims/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'hrims.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates/', 'hrims/staticfiles/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -159,6 +160,9 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
+    #os.path.join(PROJECT_ROOT, '/frontend/dist'),
+    #os.path.join(PROJECT_ROOT, 'static/frontend/dist/'),
+    #'static/frontend/dist/'
 )
 
 # Simplified static file serving.
