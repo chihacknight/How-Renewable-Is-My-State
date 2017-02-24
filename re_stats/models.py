22f3547c5d3c9140ddd3942b3fd9c04fd9c82ec0
Eric Sherman <ericandrewsherman@gmail.com>
2017-02-23 21:29:34 -0600

    remove forgein key
    overly complicated for these purposes

--- re_stats/models.py
+++ re_stats/models.py
@@ -8,8 +8,10 @@ class Data(models.Model):
     statecode = models.CharField(max_length=2)
     year = models.IntegerField()
     data = 	models.FloatField("Value for given MSN. Units may vary")
+    # def __unicode__(self):
+    #     return u'{0}'.format(self.msn)
 
 class Info(models.Model):
-    msn = models.ForeignKey( Data, unique=True)
+    msn = models.CharField("Data Series Names", max_length=5)
     description = models.CharField(max_length=255)
     detail = models.CharField(max_length=255)
