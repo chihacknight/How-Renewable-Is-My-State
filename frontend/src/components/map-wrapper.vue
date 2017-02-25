984fec3451a853a42ec19c98b7d45be8480f6089
Luke Savage <lsavage@digitalh2o.com>
2017-02-25 16:48:30 -0600

    created basic map wrapper

--- frontend/src/components/map-wrapper.vue
+++ frontend/src/components/map-wrapper.vue
@@ -1,13 +1,33 @@
 <script>
 /*
-  This will wrap a leaflet map in order to allow reusability and ease of communication.
+  Props:
+    - mapId: the id that should be assigned to the DOM element the map will mount to
+  Private variables/references:
+    - $map: a reference to the leaflet map component
 */
+import L from 'leaflet'
+
+export default {
+  props: ['mapId'],
+  mounted () {
+    this.$map = L.map(this.mapId).setView(new L.LatLng(51.3, 0.7), 9)
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(this.$map)
+  }
+}
 </script>
 
-<style>
-
+<style lang="scss">
+  .leaflet-map-wrapper {
+    height: 500px;
+    width: 1000px;
+  }
 </style>
 
 <template>
-
+  <div class="leaflet-map-wrapper">
+    <div :id="mapId"></div>
+  </div>
 </template>
