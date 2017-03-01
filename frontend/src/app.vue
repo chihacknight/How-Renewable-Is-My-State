457b8f45550d5b0e93041afc36c5b1e6f259abee
Zelda Hessler <zelda.hessler@gmail.com>
2017-03-01 11:41:01 -0600

    Add beginnings of responsive website element containers Add rudimentary site icon "RS" Add responsive header bar with links to about and faq pages Add placeholder template for side info panel Remove initial test panel that appeared on main page ("Very renewable state")

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,6 +1,9 @@
 <script>
   /* globals ENVIRONMENT,PORT */
+  import navHeader from 'components/nav-header'
   import mapWrapper from 'components/map-wrapper'
+  import sidePanel from 'components/side-panel'
+
   export default {
     data: function () {
       return {
@@ -17,28 +20,27 @@
         this.message = newMessage
       }
     },
-    components: {
-      mapWrapper
-    }
+    components: {navHeader, mapWrapper, sidePanel}
   }
 </script>
 
-<style lang="sass-loader">
-  h1 {
-    img {
-      height: 50px;
+<style lang='sass-loader'>
+  #vis-component-container {
+    width: 100%;
+    display: flex;
+    flex-flow: row nowrap;
+    sidePanel {
+      flex: 1;
     }
   }
 </style>
 
 <template>
-  <div id="app">
-    <div class="content">
-      <h1><img :src="image" />{{title}}</h1>
-      <div v-on:click="updateMessage(message)">{{message}}</div>
-      <div>Environment: {{environment}}</div>
-      <div>Port: {{port}}</div>
+  <div id='app'>
+    <navHeader></navHeader>
+    <div id='vis-component-container'>
+      <mapWrapper mapId='map'></mapWrapper>
+      <sidePanel></sidePanel>
     </div>
-    <mapWrapper mapId="map"></mapWrapper>
   </div>
 </template>
