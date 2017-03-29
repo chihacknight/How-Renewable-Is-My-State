10ee292d586728390837fca51213540a624af976
Taihua Li <taihua.ray.li@gmail.com>
2017-03-28 22:41:19 -0500

    Calculate renewable percentage instead of all

--- d3_js_test/v2/USMap.js
+++ d3_js_test/v2/USMap.js
@@ -24,7 +24,7 @@ function drawMap(geo_data) {
               .attr("height", height);
   var color = d3.scaleQuantize()
                 .domain([0, 1])
-                .range(d3.schemeYlGn[5]);;
+                .range(d3.schemeGreens[5]);;
 
   function colorMap(err, energy_data, states) {
     if (err) throw err;
@@ -37,13 +37,13 @@ function drawMap(geo_data) {
          .enter().append("path")
          .attr("d", path)
          .attr("fill", "gray")
-         .attr("stroke", "white");
+         .attr("stroke", "black");
 
     var legend_data = [0, 0.25, 0.5, 0.75, 1]; // 5 quantiles
 
     var legendBoxSettings = {
            width: 60,
-           height: 10,
+           height: 15,
            y: legendContainerSettings.y + 20
          };
 
@@ -59,6 +59,7 @@ function drawMap(geo_data) {
           .attr('y', legendBoxSettings.y)
           .attr('width', legendBoxSettings.width)
           .attr('height', legendBoxSettings.height)
+          .attr("opacity", 0.7)
           .styles({
               'fill': function(d, i){
                 return color(d);
@@ -70,31 +71,44 @@ function drawMap(geo_data) {
 
     legend.append("text")
           .attr("x", function(d, i){
-            return legendContainerSettings.x + legendBoxSettings.width * i + 30;
+            return legendContainerSettings.x + legendBoxSettings.width * i + 38;
           })
-          .attr("y", legendContainerSettings.y + 15)
+          .attr("y", legendContainerSettings.y + 30)
           .attr("font-size", 11)
           .text(function(d, i){return default_text[i];});
 
-    function updateColor(year, e_type) {
+    function updateColor(year) {
       // filter data
-      var filtered = energy_data.filter(function(d){
-              return d.year == year && d.msn == e_type;
+      var renewable_data = energy_data.filter(function(d){
+              return d.year == year && d.msn == "RETCB";
+      })
+      var total_data = energy_data.filter(function(d){
+              return d.year == year && d.msn == "TETCB";
       })
 
-      // update color scale
-      var filtered_values = [];
-      for (var i = 0; i < filtered.length; i++) {
-        filtered_values.push(filtered[i].data);
+      var renewable_values = {};
+      var total_values = {};
+      var renewable_perc = {};
+      for (var i = 0; i < total_data.length; i++) {
+        renewable_values[renewable_data[i].statecode] = renewable_data[i].data;
+        total_values[total_data[i].statecode] = total_data[i].data;
       }
 
-      var min_max = d3.extent(filtered_values);
+      var all_states = Object.keys(states);
+
+      for (var i = 0; i < all_states.length; i++) {
+        var renew_dat = renewable_values[all_states[i]];
+        var tot_dat = total_values[all_states[i]];
+        renewable_perc[all_states[i]] = renew_dat / tot_dat;
+      }
+
+      var min_max = d3.extent(d3.values(renewable_perc));
 
       if (min_max[0] == 0 && min_max[1]==0){
           alert("Sorry, data not available. Please select another energy type/year.");
       }
 
-      var normalize = d3.scaleLinear()
+      var normalize = d3.scaleSqrt()
                         .domain(min_max)
                         .range([0, 1]);
       // update color (fill)
@@ -104,58 +118,59 @@ function drawMap(geo_data) {
       // ************************************************** //
       function extractKeyValue(obj, value) {return Object.keys(obj)[Object.values(obj).indexOf(value)];};
 
-      if (year != undefined && e_type != undefined){
+      svg.selectAll("path")
+        .transition()
+        .duration(300)
+        .attr("fill", function(d){
+          state = extractKeyValue(states, parseInt(d.id));
+          if (state != undefined){
+            return color(normalize(renewable_perc[state]));
+          } else{
+            return "gray";
+          }
+        });
 
-          svg.selectAll("path")
-            .transition()
-            .duration(300)
-            .attr("fill", function(d){
-              var datum = filtered.find(function(energy_datum){
-                return energy_datum.statecode == extractKeyValue(states, parseInt(d.id));
-              })
-              if (datum != undefined){
-                return color(normalize(datum.data));
-              } else{
-                return "gray";
-              }
-            });
+      svg.selectAll("path")
+         .on("mouseover", function(){
+           d3.select(this)
+           .transition().duration(100)
+           .attr("fill", "steelblue")
+         })
+         .on("mouseout", function(){
+           d3.select(this)
+           .transition().duration(100)
+           .attr("fill", function(d){
+             state = extractKeyValue(states, parseInt(d.id));
+             if (state != undefined){
+               return color(normalize(renewable_perc[state]));
+             } else{
+               return "gray";
+             }
+         })
+       });
 
-          svg.selectAll("path")
-             .on("mouseover", function(){
-               d3.select(this)
-               .transition().duration(100)
-               .attr("fill", "steelblue")
-             })
-             .on("mouseout", function(){
-               d3.select(this)
-               .transition().duration(100)
-               .attr("fill", function(d){
-                 var datum = filtered.find(function(energy_datum){
-                   return energy_datum.statecode == extractKeyValue(states, parseInt(d.id));
-                 })
-                 if (datum != undefined){
-                   return color(normalize(datum.data));
-                 } else{
-                   return "gray";
-                 }
-             })
-           });
+        // update legend text
+        // grouped thousands with two significant digits, "4,200"
+        var formatDecimal = d3.format(".2f");
+        all_perc_values = d3.values(renewable_perc);
 
-            // update legend text
-            // grouped thousands with two significant digits, "4,200"
-            var formatDecimal = d3.format(",.3r");
+        var get_quartile = d3.scaleQuantile()
+                             .domain(min_max)
+                             .range([0, 1, 2, 3]);
 
-            var legend_text = [formatDecimal(d3.min(filtered_values)),
-                             '>'+formatDecimal(d3.quantile(filtered_values, 0.25)),
-                             '>'+formatDecimal(d3.quantile(filtered_values, 0.5)),
-                             '>'+formatDecimal(d3.quantile(filtered_values, 0.75)),
-                             '>'+formatDecimal(d3.max(filtered_values))];
+        all_perc_values.sort(function(a, b){return a-b});
+        var legend_text = ['min: ' + formatDecimal(d3.min(all_perc_values)*100),
+                         'Q1: ' + formatDecimal(d3.quantile(all_perc_values, 0.25)*100) ,
+                         'Med: ' + formatDecimal(d3.quantile(all_perc_values, 0.5)*100) ,
+                         'Q3: ' + formatDecimal(d3.quantile(all_perc_values, 0.75)*100) ,
+                         'max: ' + formatDecimal(d3.max(all_perc_values)*100)
+                          ];
 
-            var new_legend = svg.selectAll('text');
+        var new_legend = svg.selectAll('text');
 
-            new_legend.text(function(d, i) {return legend_text[i];});
+        new_legend.text(function(d, i) {return legend_text[i];});
 
-        } // end of the if statement to check year & e_type are defined
+
      } // end of function updateColor
 
      // change map with drop down selections
